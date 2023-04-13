26a52a5bf4c1c4fb7e1f59d7217eecf24a8a1306
MemoGarciaC <memo.g.cano@gmail.com>
2023-04-13 15:39:06 -0600

    Remove unnecessary spotipy instance creation

--- spotify_playlist.py
+++ spotify_playlist.py
@@ -6,16 +6,16 @@ CLIENT_ID = 'fba9c64e3fd7465699d57c02c746fe88'
 CLIENT_SECRET = '6b6601dfbaa54706b64f89fa82f55130'
 REDIRECT_URI = os.environ.get("REDIRECT_URI", "http://localhost:5000/callback/")
 
+sp_oauth = spotipy.SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope="user-top-read playlist-modify-public",
+    show_dialog=True
+)
+
 def create_top_songs_playlist(access_token):
-    sp_oauth = SpotifyOAuth(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI,
-        scope="user-top-read playlist-modify-public",
-        show_dialog=True,
-        token_info={"access_token": access_token}  # Add this line to provide the user's access token
-    )
-    sp = spotipy.Spotify(auth_manager=sp_oauth)
+    sp = spotipy.Spotify(auth=access_token)
 
     top_tracks = sp.current_user_top_tracks(limit=5, time_range='long_term')
     track_ids = [track['id'] for track in top_tracks['items']]
