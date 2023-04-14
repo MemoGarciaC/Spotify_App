fb543db7bb7941eaa830729fe8e72bef48a544cf
MemoGarciaC <memo.g.cano@gmail.com>
2023-04-14 09:37:02 -0600

    Fix playlist creation in user's Spotify account

--- spotify_playlist.py
+++ spotify_playlist.py
@@ -5,9 +5,9 @@ def create_top_songs_playlist(user_id, sp):
     top_tracks = sp.current_user_top_tracks(limit=5, time_range='long_term')
     track_ids = [track['id'] for track in top_tracks['items']]
 
-    recommendations = sp.recommendations(seed_tracks=track_ids, limit=50)
+    recommendations = sp.recommendations(seed_tracks=track_ids, limit=50, market='US')
 
-    playlist_name = "Prueba 1.1"
+    playlist_name = "Prueba 1.2"
     new_playlist = sp.user_playlist_create(user_id, playlist_name)
 
     playlist_id = new_playlist['id']
